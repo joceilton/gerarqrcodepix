7018fd0296cdbd225bb4bfc847ad0c7770c21ab9
joceilton <joceilton.g@gmail.com>
2022-06-21 08:58:05 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,9 +15,8 @@ $(function() {
 	$(".form-get-qrcode-pix").on("submit", function(e) {
 		e.preventDefault()
 		$.ajax({
-			url: "https://pagmp.com/pix/index.php?",
-			method: "GET",
-			data: { valor: $(".valor").val(), nome: $(".nome").val(), cidade: $(".cidade").val(), chave: $(".chave").val(), fone: $(".fone").val() }
+			url: "https://pagmp.com/pix/index.php?valor=10,00&nome=Deivid%20Rothen&cidade=Porto%20Alegre&chave=bc0addbb-5bd1-4726-a962-7e48b4eb33c3&fone=5548991174663",
+			method: "GET"
 		}).done(function(data) {
 			console.log(data)
 		})
